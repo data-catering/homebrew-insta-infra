a0f8ec6faa00c509d81f1545c8c805dc61655621
Flook Peter <pflooky@hotmail.com>
2025-03-27 11:40:49 +0800

    Update to use the go version of insta-infra

--- Formula/insta-infra.rb
+++ Formula/insta-infra.rb
@@ -1,15 +1,17 @@
 class InstaInfra < Formula
   desc "Spin up any service straight away on your local laptop"
   homepage "https://github.com/data-catering/insta-infra"
-  url "https://github.com/data-catering/insta-infra/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/data-catering/insta-infra/archive/refs/tags/v2.0.1.tar.gz"
   sha256 "24a1928e5a81d936ba3e01e3ebf3ed214eadfc55a81178e56d668a7e1968a687"
   license "MIT"
 
   depends_on "docker" => :recommended
   depends_on "docker-compose" => :recommended
+  depends_on "go" => :build
 
   def install
-    bin.install "run.sh" => "insta"
+    system "go", "install", "github.com/data-catering/insta-infra/v2/cmd/insta@v2.0.1"
+    bin.install "insta"
     prefix.install "docker-compose.yaml"
     prefix.install "docker-compose-persist.yaml"
     prefix.install "data"
